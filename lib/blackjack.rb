3ca44ade9340484cc86ff1984744640502c40636
Ali Madni <ali555m@hotmail.com>
2019-02-09 21:56:38 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -34,7 +34,7 @@ def hit?(num)
   choice = get_user_input
   if choice != "s" && choice != "h"
     invalid_command
-  elsif choice = h
+  elsif choice = "h"
     num += deal_card()
   else
     num
